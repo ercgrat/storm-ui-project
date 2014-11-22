8449fb9b17cb058ae3c2a2786392a34e77e68bde
ercgrat <ercgrat@gmail.com>
2014-11-22 15:29:58 -0500

    Sorting Table Controller
    The sorting table feature is complete. It is defined by the
    <output-table> directive.

--- app.js
+++ app.js
@@ -1,31 +1,94 @@
 (function() {
     var module = angular.module('stormModule', []);
+    var indexKey = "___ANGULARJS_STORM_MODULE_INDEX_";
+    var inTuples = [
+        {
+            time: 3.20,
+            y1: 2,
+            y2: 7
+        },
+        {
+            time: 2.98,
+            y1: 3,
+            y2: 8
+        },
+        {
+            time: 3.10,
+            y1: 2,
+            y2: 8
+        },
+        {
+            time: 2.55,
+            y1: 6,
+            y2: 3
+        },
+        {
+            time: 4.01,
+            y1: 10,
+            y2: -1
+        }
+    ];
     
-    module.directive('outputTable', ['$sce', function($sce) {
+    var tuples = indexTuples(inTuples, indexKey);
+    
+    function indexTuples(inTuples, indexKey) {
+        var tuples = [];
+        var tupleIndex = 0;
+        for(tupleIndex in inTuples){
+            tuples.push({});
+            tuples[tupleIndex][indexKey] = tupleIndex;
+            for(keyIndex in Object.keys(inTuples[tupleIndex])){
+                var key = Object.keys(inTuples[tupleIndex])[keyIndex];
+                tuples[tupleIndex][key] = inTuples[tupleIndex][key];
+            }
+            tupleIndex++;
+        }
+        return tuples;
+    }
+    
+    
+    module.directive('outputTable', ['$sce', '$filter', function($sce, $filter) {
         return {
             restrict: 'E',
             templateUrl: '/output-table.html',
             controller: function() {
-                this.tuples = [
-                {
-                    time: 3.20,
-                    y1: 2,
-                    y2: 7
-                },
-                {
-                    time: 2.98,
-                    y1: 3,
-                    y2: 8
+                this.sortKey = Object.keys(tuples[0])[0]; // Gets first key of first tuple
+                this.sortDescending = false;
+                this.tuples = tuples;
+                
+                this.getKey = function(index){
+                    if(index === 0) {
+                        return "#";
+                    } else {
+                        return Object.keys(this.tuples[0])[index];
+                    }
                 }
-            ];
+                
+                this.setSortKey = function(sortKey) {
+                    if(sortKey === this.sortKey) {
+                        this.sortDescending = !this.sortDescending;
+                    } else {
+                        this.sortKey = sortKey;
+                        this.sortDescending = false;
+                    }
+                    this.tuples = $filter('orderBy')(tuples,this.sortKey,this.sortDescending);
+                };
+                
+                this.getHeaderClassForKey = function(sortKey) {
+                    var headerClass = "glyphicon ";
+                    if(sortKey === this.sortKey) {
+                        if(this.sortDescending) {
+                            headerClass += "glyphicon-chevron-down";
+                        } else {
+                            headerClass += "glyphicon-chevron-up";
+                        }
+                    } else {
+                        headerClass += "glyphicon-minus";
+                    }
+                    return headerClass;
+                };
             },
             controllerAs: 'outputCtrl'
         };
     }]);
-    
-    module.filter('greet', function(){
-        return function(name) {
-            return "Hello, " + name + "!";
-        }
-    });
 })();
