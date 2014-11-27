2abbb65d89f3e811af811352a7302002b6f7bf9a
Eric Gratta <ercgrat@gmail.com>
2014-11-26 23:27:17 -0500

    Established functioning websocket connection between app.js and php scripts using Ratchet.

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 (function() {
     var module = angular.module('app', ['app.sortingTableModule', 'app.stormServiceModule', 'app.chartModule']);
     
-    module.controller('PageController', function(){
+    module.controller('PageController', ["$timeout", function($timeout){
         this.pageSelected = 0;
-        var eventSource = new EventSource("get_data.php");
-        eventSource.onmessage = function(msg) {
-            var newElement = document.createElement("li");
-            newElement.innerHTML = "count: " + msg.data;
-            $.getElementById("test_list").appendChild();
-        }
+	var stormConn = new WebSocket("ws://localhost:8080");
+
+	stormConn.onopen = function(msg) {
+		console.log("Successful connection via ws.");
+		$timeout(function(){ stormConn.send("Hello, world!"); }, 2000);
+	}
+	stormConn.onmessage = function(msg) {
+		console.log("Data: " + msg.data);
+	}
         
         this.pageSelectedIs = function(page) {
             if(page === this.pageSelected){
@@ -18,7 +21,7 @@
         this.selectPage = function(page) {
             this.pageSelected = page;
         }
-    })
+    }]);
     
     // Manually boostrap angular after loading the Google Chart API
     google.setOnLoadCallback(function() {
--- get_data.php
+++ get_data.php
@@ -1,20 +1,47 @@
 <?php
-// Specify MIME Type to be a text/event stream for HTML5 server-side events
-header("Content-type: text/event-stream\n\n");
 
 // Require Redis autoloader and register with the Redis server
 require '/home/eric/Predis/vendor/predis/predis/autoload.php';
 Predis\Autoloader::register();
 
+// Use Ratchet classes for communicating over web sockets
+use Ratchet\MessageComponentInterface;
+use Ratchet\ConnectionInterface;
+use Ratchet\Server\IoServer;
+use Ratchet\Http\HttpServer;
+use Ratchet\WebSocket\WsServer;
+require '/home/eric/Ratchet/vendor/autoload.php';
+
 // Create new Predis client
 $client = new Predis\Client();
-$i = 0;
-while(true) {
-	echo "data" . $i . "\n\n";
+
+// Define communication class
+class StormConnection implements MessageComponentInterface {
+	protected $clients;
+
+	public function __construct() {
+		$this->clients = new \SplObjectStorage;
+	}
+
+	public function onOpen(ConnectionInterface $conn) {
+		$this->clients->attach($conn);
+		echo "New connection!";		
+	}
+
+	public function onMessage(ConnectionInterface $from, $msg) {
+		$from->send("HEY~!");		
+	}
 	
-	ob_flush();
-	flush();
-	sleep(4);
+	public function onClose(ConnectionInterface $conn) {
+		
+	}
+
+	public function onError(ConnectionInterface $conn, \Exception $e) {
+		
+	}
 }
+
+$server = IoServer::factory(new HttpServer(new WsServer(new StormConnection())), 8080);
+$server->run();
 
 ?>
